d190e49b93befd2a81b6a9ba1be502d7eccf2ec1
David Lgc <legouellecd@gmail.com>
2023-09-20 00:34:56 +0200

    Add templates

---
+++ a_propos.php
@@ -0,0 +1,8 @@
+<?php 
+
+require_once __DIR__ ."/templates/header.php"; ?>
+
+    <h1>A propos</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis temporibus dicta, cupiditate asperiores sunt odio illum facere totam numquam, possimus eaque officiis sint facilis!</p>
+
+<?php require_once __DIR__ ."/templates/footer.php"; ?>
