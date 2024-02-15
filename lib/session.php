392ec32d55386dc545d883f74eae23004560a42e
David Lgc <legouellecd@gmail.com>
2024-02-15 16:34:04 +0100

    Add AdminOnly()

--- lib/session.php
+++ lib/session.php
@@ -1,11 +1,21 @@
 <?php
 session_set_cookie_params([
-    
-    'lifetime' => 3600,
-    'path' => '/',
-    'domain' => '_DOMAIN_',
-    // 'secure' => true,
-      'httponly' => true //--évite les cookie manipulable en js
+
+  'lifetime' => 3600,
+  'path' => '/',
+  'domain' => '_DOMAIN_',
+  // 'secure' => true,   necessite https
+  'httponly' => true //--évite les cookie manipulable en js
 ]);
+
+function adminOnly()
+{
+  if (!isset($_SESSION['user'])) {
+    header('location: ../login.php');
+  } elseif ($_SESSION['user']['role'] != 'admin') {
+
+    header('location: ../index.php');
+  }
+}
 
 session_start();
