623db03575ae5ff9cbcd3a0d0899d62f906be219
David Lgc <legouellecd@gmail.com>
2024-02-16 15:37:06 +0100

    Fix default-pic for article without pic

--- lib/config.php
+++ lib/config.php
@@ -1,9 +1,11 @@
 <?php
 
+define("_DOMAIN_", "localhost");
 define("_DB_SERVER_", "localhost");
 define("_DB_NAME_", "manga-trendz");  
 define("_DB_USER_", "root");
 define("_DB_PASSWORD_", "");
 define("_ASSETS_IMAGES_FOLDER_", "/assets/img/");//  peut etre a echanger
-define("_ARTICLES_IMAGES_FOLDER_", "/uploads/articles/");
+define("_ARTICLES_IMAGES_FOLDER_", "assets/uploads/articles/");
 define("_HOME_ARTICLES_LIMIT_", 3);
+define("_ADMIN_ITEM_PER_PAGE_", 10);
