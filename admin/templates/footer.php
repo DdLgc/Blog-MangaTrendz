82a9d8355ea60704683f5ace1dfebf7ef2674e70
David Lgc <legouellecd@gmail.com>
2024-02-17 14:47:35 +0100

    Manage admin dashboard

--- admin/templates/footer.php
+++ admin/templates/footer.php
@@ -1,4 +1,3 @@
-
 </main>
 
 
@@ -6,11 +5,13 @@
 </div>
 
 <footer class="py-3 my-4">
-    <p class="text-center">Admin Manga</p>
+    <p class="text-center">Admin MangaTrendz</p>
 </footer>
 
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous">
+</script>
 </body>
 
 </html>
