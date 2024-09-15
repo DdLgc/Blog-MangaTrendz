c6bdb719f12e4d19bd0f802c8eaa2a9c0480ad25
David Lgc <legouellecd@gmail.com>
2024-09-15 19:22:53 +0200

    Re update nav-kinks footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,18 +1,32 @@
 </main>
 
-<footer class="py-3 my-4">
-    <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-        <?php foreach ($mainMenu as $key => $menuItem) {
-            if (!array_key_exists("exclude", $menuItem)) { ?>
-                <li class="nav-item">
-                    <a href="<?= $key; ?>" class="nav-link"><?= $menuItem["menu_title"]; ?></a>
-                </li>
-        <?php }
-        } ?>
-    </ul>
-    <p class="text-center text-body-secondary">© <?= date("Y") ?> DdLgc</p>
-</footer>
+<footer class="py-4 my-4 border-top">
+    <div class="container d-flex justify-content-between">
+        <ul class="nav flex-column">
+            <li class="nav-item">
+                <a href="contact.php" class="nav-link px-0 py-1 footer-link">Nous contacter</a>
+            </li>
+            <li class="nav-item">
+                <a href="mentions-legales.php" class="nav-link px-0 py-1 footer-link">Mentions légales</a>
+            </li>
+            <li class="nav-item">
+                <a href="politique-confidentialite.php" class="nav-link px-0 py-1 footer-link">Politique de confidentialité</a>
+            </li>
+        </ul>
 
+        <div class="d-flex align-items-center justify-content-center">
+            <a href="/">
+                <img src="assets/img/logo.jpg" alt="Logo MangaTrendz" class="footer-logo" width="150">
+            </a>
+        </div>
+
+        <div class="d-flex flex-column align-items-end">
+            <p class="text-muted">Bienvenue sur MangaTrendz, votre source d'actualités sur les mangas et les animés. Restez à jour avec nous !</p>
+        </div>
+    </div>
+
+    <p class="text-center text-muted">© <?= date("Y") ?> DdLgc</p>
+</footer>
 </div>
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
