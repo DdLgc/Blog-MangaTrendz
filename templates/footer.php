cece32b43b679e2ff2ef2e8df89c8d84b675d5a5
David Lgc <legouellecd@gmail.com>
2024-09-15 17:06:40 +0200

    Update nav-link footer

--- templates/footer.php
+++ templates/footer.php
@@ -2,14 +2,15 @@
 
 <footer class="py-3 my-4">
     <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Home</a></li>
-        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Features</a></li>
-        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Pricing</a></li>
-        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">FAQs</a></li>
-        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">About</a></li>
+        <?php foreach ($mainMenu as $key => $menuItem) {
+            if (!array_key_exists("exclude", $menuItem)) { ?>
+                <li class="nav-item">
+                    <a href="<?= $key; ?>" class="nav-link"><?= $menuItem["menu_title"]; ?></a>
+                </li>
+        <?php }
+        } ?>
     </ul>
-    <?php $annee = date("Y");?>
-    <p class="text-center text-body-secondary">© <?php echo $annee ?> DdLgc</p>
+    <p class="text-center text-body-secondary">© <?= date("Y") ?> DdLgc</p>
 </footer>
 
 </div>
