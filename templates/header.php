c8719e3a87ba105a23a7ea61df677b95d3a5c730
David Lgc <legouellecd@gmail.com>
2024-09-19 18:50:01 +0200

    add favicon to header.php

--- templates/header.php
+++ templates/header.php
@@ -12,7 +12,7 @@ $currentPage = basename($_SERVER["SCRIPT_NAME"]);
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="description" content="<?= $mainMenu[$currentPage]["meta_description"] ?>">
   <title><?= $mainMenu[$currentPage]["head_title"] ?></title>
-
+  <link rel="icon" href="assets/img/Logo.jpg" type="image/jpg">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
