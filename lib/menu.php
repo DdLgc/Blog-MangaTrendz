2aaefbf5e5d1619a60ab22ebc7e36799492e70f7
David Lgc <legouellecd@gmail.com>
2024-09-21 18:38:32 +0200

    update links, files, menu for footer

--- lib/menu.php
+++ lib/menu.php
@@ -6,6 +6,6 @@ $mainMenu = [
     "a_propos.php" => ["menu_title" => "A propos", "head_title" => "A propos de MangaTrendz", "meta_description" => "L'histoire du site MangaTrendz"],
     "login.php" => ["menu_title" => "Connexion", "head_title" => "Connexion MangaTrendz", "meta_description" => "Connexion MangaTrendz", "exclude" => true],
     "contact.php" => ["menu_title" => "Nous contacter", "head_title" => "Nous contacter", "meta_description" => "Nous contacter", "exclude" => true],
-    "mentions_légales.php" => ["menu_title" => "Mentions Légales", "head_title" => "Mentions Légales", "meta_description" => "Mentions Légales", "exclude" => true],
+    "legal_information.php" => ["menu_title" => "Mentions Légales", "head_title" => "Mentions Légales", "meta_description" => "Mentions Légales", "exclude" => true],
     "pdc.php" => ["menu_title" => "Politique de confidentialité", "head_title" => "Politique de confidentialité", "meta_description" => "Politique de confidentialité", "exclude" => true],
 ];
