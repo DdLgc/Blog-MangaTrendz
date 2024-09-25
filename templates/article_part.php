f691b3fa6a540a887b30ceb8293d29c04d0132fc
David Lgc <legouellecd@gmail.com>
2024-09-25 23:16:24 +0200

    Update css article style

--- templates/article_part.php
+++ templates/article_part.php
@@ -7,7 +7,13 @@ $imagePath = getArticleImage($article["image"]);
         <img src="<?= $imagePath ?>" class="card-img-top img-fluid img-thumbnail" alt="<?= htmlentities($article["title"]) ?>" style="height: 200px; object-fit: cover;">
         <div class="card-body d-flex flex-column">
             <h5 class="card-title"><?= htmlentities($article["title"]) ?></h5>
-            <p class="card-text"><?= htmlentities(substr($article["content"], 0, 100)) ?>...</p>
+            <?php if (isset($article['class']) && $article['class'] === 'featured-article') : ?>
+
+                <p class="card-text"><?= htmlentities($article["content"]) ?></p>
+            <?php else : ?>
+
+                <p class="card-text"><?= htmlentities(substr($article["content"], 0, 100)) ?>...</p>
+            <?php endif; ?>
             <a href="actualite.php?id=<?= $article["id"] ?>" class="button btn btn-primary mt-auto">Lire la suite</a>
         </div>
     </div>
