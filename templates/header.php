3245e2ced502536d427bebf622cf105cbe60a375
David Lgc <legouellecd@gmail.com>
2026-02-16 15:18:22 +0100

    fix: correction responsive header entre 768-992px

--- templates/header.php
+++ templates/header.php
@@ -25,8 +25,8 @@ $currentPage = basename($_SERVER["SCRIPT_NAME"]);
 
 <body>
   <div class="container">
-    <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
-      <div class="col-md-3 mb-2 mb-md-0">
+    <header class="d-flex flex-column flex-md-row align-items-center justify-content-between py-3 mb-4 border-bottom">
+      <div class="col-12 col-md-3 text-center text-md-start mb-3 mb-md-0">
         <a href="/" class="d-inline-flex link-body-emphasis text-decoration-none">
           <img src="assets/img/logo.jpg" alt="logo MangaTrendz" width="150" class="logo d-block mx-lg-auto img-fluid blinking-logo">
         </a>
@@ -43,7 +43,7 @@ $currentPage = basename($_SERVER["SCRIPT_NAME"]);
         ?>
       </ul>
 
-      <div class="col-md-3 text-end">
+      <div class="col-12 col-md-3 text-center text-md-end mt-3 mt-md-0">
         <?php if (isset($_SESSION["user"])) { ?>
           <a href="logout.php" class="button btn btn-primary">Déconnexion</a>
         <?php } else { ?>
